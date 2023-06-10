70584e88f16039c0c48e4fd3489afdea5fd2df36
Loan Duyên <43545283+duyenlan151@users.noreply.github.com>
2023-06-10 09:39:11 +0700

    [TES] test route

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,7 +1,34 @@
 // path: src/router/routes.ts
 import { RouteRecordRaw } from "vue-router";
+import { homePath, productsPath, loginPath } from "./paths";
 import { home, products } from "./routers";
 
-const routes: Array<RouteRecordRaw> = [{ ...home }, { ...products }];
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: homePath,
+    name: "Home",
+    component: () =>
+      import(/* webpackChunkName: "home" */ "@/views/Home/Home.vue"),
+    meta: {
+      layout: "LayoutMain",
+    },
+  },
+  {
+    path: productsPath,
+    name: "products",
+    component: async () =>
+      await import(
+        /* webpackChunkName: "products" */ "@/views/Products/Products.vue"
+      ),
+  },
+  {
+    path: loginPath,
+    name: "login",
+    component: async () =>
+      await import(
+        /* webpackChunkName: "products" */ "@/views/Login/Login.vue"
+      ),
+  },
+];
 
 export default routes;
